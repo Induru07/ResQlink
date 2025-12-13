06debd8b1d12ac5382009309ff0abc2df8066321
W.G.I.Adeesha <induruadeesha2356@gmail.com>
2025-12-13 22:40:17 +0530

    Update config.js

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -2,6 +2,9 @@
 (function(){
   var host = window.location.hostname || '';
   var renderApi = 'https://resqlink-ovm6.onrender.com';
-  var isProd = /resqlink\.org$|resqlink\.com$/i.test(host) || /onrender\.com$/i.test(host);
+  var isRenderFront = /resqlink-1-dt40\.onrender\.com$/i.test(host);
+  var isCustomDomain = /resqlink\.org$|resqlink\.com$/i.test(host);
+  var isAnyRender = /onrender\.com$/i.test(host);
+  var isProd = isCustomDomain || isAnyRender;
   window.API_BASE = isProd ? renderApi : 'http://localhost:5000';
 })();
